b82a734842472808c56959f032d348356a267465
Dell <ricardo.lagoshn@gmail.com>
2025-10-25 20:51:28 -0600

    Cambios al 25/10/2025 Fotografia y permisos

--- app/src/main/java/com/example/ucenm3p20251/MainActivity.java
+++ app/src/main/java/com/example/ucenm3p20251/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.ucenm3p20251;
 
+import android.Manifest;
 import android.content.ContentValues;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -18,12 +24,18 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.ucenm3p20251.configuraciones.SQLiteConexion;
 import com.example.ucenm3p20251.configuraciones.Transacciones;
 
+import java.io.File;
+
 public class MainActivity extends AppCompatActivity {
 
     // Declaracion de variables globales
     EditText nombres, apellidos, edad, correo;
+    ImageView imageView;
+    Button btnagregar, btnfoto;
 
-    Button btnagregar;
+    private static final int PERMISO_CAMARA = 101;
+    private String fotoBase64 = null;
+    private File fotoFile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +48,10 @@ public class MainActivity extends AppCompatActivity {
         apellidos = (EditText) findViewById(R.id.apellidos);
         edad = (EditText) findViewById(R.id.edad);
         correo = (EditText) findViewById(R.id.correo);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
         btnagregar = (Button) findViewById(R.id.btnagregar);
+        btnfoto = (Button) findViewById(R.id.btnfoto);
 
         btnagregar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -57,7 +72,48 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+        btnfoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Permisos();
+            }
+        });
 
+
+
+    }
+
+    private void Permisos() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) !=
+                PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{ Manifest.permission.CAMERA}, PERMISO_CAMARA);
+        }
+        else
+        {
+            OpenCamara();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults, int deviceId) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults, deviceId);
+
+        if(requestCode == PERMISO_CAMARA)
+        {
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+            {
+                OpenCamara();
+            }
+            else
+            {
+                Toast.makeText(this, "Permiso de Camara denegado "  , Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    private void OpenCamara() {
     }
 
     private void AddPersona()
